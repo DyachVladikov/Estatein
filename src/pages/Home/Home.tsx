ce3f86a031cf17b64d1fdf3d3bfd2b0d05c71a5d
Vladislav <dychvladikov08@gmail.com>
2026-01-25 15:01:44 +0300

    Variables

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import "./Home.scss"
-import React from "react"
 
 
 const Home = () => {
     return (
-        <>
+        <div className="home">
             Home
-        </>
+        </div>
+        
     )
 }
 
