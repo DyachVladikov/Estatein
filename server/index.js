718391a8424f300ddf34ed1187f0f716fb2c67c5
Dyach_Vladikov <dyachvladikov08@gmail.com>
2026-06-03 15:10:12 +0300

    fix cors 3

--- server/index.js
+++ server/index.js
@@ -14,23 +14,9 @@ dotenv.config();
 
 const app = express();
 
-//app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }))
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 //app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Перехватываем предварительный запрос браузера и сразу отдаем статус 200
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 app.use("/api", Routes);
 
 mongoose
